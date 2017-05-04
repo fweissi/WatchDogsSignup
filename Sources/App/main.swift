f073cfc8794a920e4f8a16781bd3bea2ab6e4bc4
Keith Weiss <fweissi@me.com>
2017-05-04 01:17:39 -0600

    Added PostgreSQL support to my app.

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,12 +1,25 @@
 import Vapor
+import VaporPostgreSQL
 
 let drop = Droplet()
+try drop.addProvider(VaporPostgreSQL.Provider)
+
 
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"],
     	"info": drop.localization[req.lang, "welcome", "subtitle"]
     ])
+}
+
+drop.get("version") { request in
+    if let db = drop.database?.driver as? PostgreSQLDriver {
+        let version = try db.raw("SELECT version()")
+        return try JSON(node: version)
+    }
+    else {
+        return "No DB Connection"
+    }
 }
 
 drop.resource("posts", PostController())
